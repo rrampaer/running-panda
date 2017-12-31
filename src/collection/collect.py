6ff25d3a75d963cdc79c077b869bfcede559b57d
rrampaer <robin.rampaer@gmail.com>
2017-12-31 14:29:47 +0100

    Replace deep recursion

--- src/collection/collect.py
+++ src/collection/collect.py
@@ -3,6 +3,9 @@ import csv
 import requests
 from http.cookies import SimpleCookie
 
+# Relative path to folder in which to save the CSVs
+PATH_TO_CSV_FOLDER = 'data/activities'
+
 # To get started open garmin connect in Google Chrome, log in and grab the id of your latest activity as in https://connect.garmin.com/modern/activity/<START_ID>
 START_ID = 2399797019 # Paste it here
 END_ID = 0 # Id to stop at if you've run this script before, leave 0 otherwise
@@ -22,15 +25,6 @@ headers = {
     'From': 'you@domain.com'
 } 
 
-def get_next_activity_id(activity_id):
-    url = 'https://connect.garmin.com/modern/proxy/activity-service/activity/{}/relative'.format(activity_id)
-    r = requests.get(url, headers=headers, cookies=cookies)
-    try:
-        next_activity_id =  r.json()["nextActivityId"]
-        return(next_activity_id)
-    except KeyError as KeyError:
-        print("Issue getting ")
-
 
 def get_previous_activity_id(activity_id):
     url = 'https://connect.garmin.com/modern/proxy/activity-service/activity/{}/relative'.format(activity_id)
@@ -40,28 +34,25 @@ def get_previous_activity_id(activity_id):
         return(previous_activity_id)
     except KeyError:
         print('Issue getting the key ["previousActivityId"] for activity with id {}'.format(activity_id))
-        return KeyError
+        return None
 
 
 def export_activity_laps(activity_id):
     url = 'https://connect.garmin.com/modern/proxy/download-service/export/csv/activity/{}'.format(activity_id)
     r = requests.get(url, headers=headers, cookies=cookies)
     try:
-        with open('data/activities/{}.csv'.format(activity_id), 'w') as temp_file:
+        with open('{}/{}.csv'.format(PATH_TO_CSV_FOLDER, activity_id), 'w') as temp_file:
             temp_file.writelines(r.content.decode('utf-8'))
-        return True
     except Exception as e:
         print('Issue writing the csv file for activity {}'.format(activity_id))
         return e
 
 
 def export(activity_id):
-    previous_id = get_previous_activity_id(activity_id)
-    export_activity_laps(activity_id)
-    if previous_id and previous_id != END_ID :
-        export(previous_id)
-    else:
-        print("Finished exporting")
+    while activity_id and activity_id != END_ID :
+        export_activity_laps(activity_id)
+        activity_id = get_previous_activity_id(activity_id)
+    print("Finished exporting")
 
 
 def main():
